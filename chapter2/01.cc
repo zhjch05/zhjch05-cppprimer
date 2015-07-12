4b4925237618c7cfc9412558dd79688ee670af8f
Jincheng <zjc1996@brandeis.edu>
2015-07-12 14:45:51 -0400

    init

---
+++ chapter2/01.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+  unsigned char c = -1;
+  cout<<c;
+  return 0;
+}
---
+++ chapter2/note.cc
@@ -0,0 +1,10 @@
+unsigned char c = -1;
+cout<<c<<endl; //\377
+
+Type conversion:
+non-bool -> bool: 0 -> false, other -> true
+bool -> arithmetic: true -> 1, false -> 0
+float -> int: value is truncated, 1.6 -> 1
+int -> float: 1 -> 1.0
+out-of-range -> unsigned :8-bit unsigned char = -1 -> 255
+out-of-range -> signed: undefined(overflow)
